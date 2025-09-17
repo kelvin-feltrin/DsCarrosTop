329c0a027daa9054415b2caaf28aef1f4fd921a2
kelvin-feltrin <kelvin-798@hotmail.com>
2025-09-17 01:01:32 -0300

    Criacao do Card de carro

--- src/Routes/Catalog/index.tsx
+++ src/Routes/Catalog/index.tsx
@@ -2,13 +2,24 @@ import './styles.css';
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import CardCar from '../../components/CardCar';
 
 export default function Catalog() {
     return (
-        <>
+        <div className='ct-page-container'>
             <Header />
-            
+
+            <main className='ct-catalog-main'>
+                <section className='ct-section-card-cars ct-container'>
+                    <h2>Venha nos visitar</h2>
+                    <div className='ct-card-cars-container'>
+                        <CardCar />
+                        <CardCar />
+                    </div>
+                </section>
+            </main>
+
             <Footer />
-        </>
+        </div>
     );
 }
